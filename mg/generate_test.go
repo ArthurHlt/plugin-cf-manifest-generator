f0a0f435aafe8ebd490540c991e33f18ba9557ac
ArthurHlt <arthurh.halet@gmail.com>
2015-11-11 06:01:17 +0100

    add test script

--- mg/generate_test.go
+++ mg/generate_test.go
@@ -1,10 +1,10 @@
 package mg_test
 
 import (
-	. "github.com/ArthurHlt/plugin-cf-manifest-generator/mg"
+//. "github.com/ArthurHlt/plugin-cf-manifest-generator/mg"
 
 	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+//. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Generate", func() {
